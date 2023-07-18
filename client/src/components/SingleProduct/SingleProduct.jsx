1308dae49de17b63227df462f4cb605368d47375
sonal1729 <sonal.kumar2019@vitbhopal.ac.in>
2023-07-18 17:24:01 +0530

    Add the category and products page

--- client/src/components/SingleProduct/SingleProduct.jsx
+++ client/src/components/SingleProduct/SingleProduct.jsx
@@ -1,6 +1,23 @@
 import "./SingleProduct.scss";
+import {
+    FaFacebookF, FaTwitter, FaInstagram, FaLinkedinIn, FaPinterest, FaCartPlus
+} from "react-icons/fa";
+import React from "react";
+import prod from "../../assets/products/earbuds-prod-1.webp"
+
 const SingleProduct = () => {
-    return <div>Single Product</div>;
+    return <div className="single-product-container">
+        <div className="layout">
+            <div className="single-product-page">
+               <div className="left">
+                <img src={prod} alt=""/>
+               </div>
+               <div className="right">
+
+               </div>
+            </div>
+        </div>
+    </div>;
 };
 
 export default SingleProduct;
